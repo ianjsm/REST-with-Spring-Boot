fb7434cba151d73f886c94a825ea7855f3433c4c
Ian Soares <ian.soares@aluno.uece.br>
2024-03-25 20:12:43 -0300

    First commit

---
+++ src/test/java/com/ian/springboot/StartupTests.java
@@ -0,0 +1,13 @@
+package com.ian.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
